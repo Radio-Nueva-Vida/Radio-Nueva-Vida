7317f107a538f15e853d4bfc36e284a871933bc1
Radio-Nueva-Vida <mentoplush@hotmail.com>
2025-03-19 12:20:36 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,3 +2,12 @@ $(document).ready(function() {
     console.log("Página de Radio Nueva Vida cargada correctamente.");
     // Puedes agregar más interactividad aquí.
 });
+$(document).ready(function() {
+    const audioPlayer = $('audio')[0]; // Selecciona el reproductor de audio
+    audioPlayer.onplay = function() {
+        console.log("Reproducción iniciada");
+    };
+    audioPlayer.onpause = function() {
+        console.log("Reproducción pausada");
+    };
+});
