09afefaacb7d631d5c5509d26378857193e9cf93
Radio-Nueva-Vida <mentoplush@hotmail.com>
2025-10-11 13:10:25 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+
+const audio = document.getElementById("audio");
+const playPauseBtn = document.getElementById("playPause");
+const iconoPlay = document.getElementById("iconoPlay");
+const iconoPause = document.getElementById("iconoPause");
+const volumen = document.getElementById("volumen");
+
+const artista = document.getElementById("artista");
+const titulo = document.getElementById("titulo");
+const albumArt = document.getElementById("album-art");
+
+let isPlaying = false;
+
+// URL del proxy en Render
+const URL_PROXY = "https://proxy-metadatos.onrender.com/metadata";
+
+// Reproducir / Pausar
+playPauseBtn.addEventListener("click", () => {
+  if (isPlaying) {
+    audio.pause();
+    iconoPlay.style.display = "inline";
+    iconoPause.style.display = "none";
+  } else {
+    audio.play();
+    iconoPlay.style.display = "none";
+    iconoPause.style.display = "inline";
+  }
+  isPlaying = !isPlaying;
+});
+
+// Control de volumen
+volumen.addEventListener("input", () => {
+  audio.volume = volumen.value;
+});
+
+// Limpiar posibles etiquetas HTML o basura en el texto recibido
